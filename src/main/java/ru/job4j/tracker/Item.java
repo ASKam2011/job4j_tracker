7b4b0a59de2edf15bf8ffd6e20da5c37742a0297
Admin <20nastya93@mail.ru>
2022-11-24 11:04:12 +0700

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -41,10 +41,6 @@ public class Item {
 
     @Override
     public String toString() {
-        return "Item{" +
-                "created=" + created.format(FORMATTER) +
-                ", id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        return "Item{" + "created=" + created.format(FORMATTER) + ", id=" + id + ", name='" + name + '\'' + '}';
     }
 }
